8d2be66aaf60424851b12475a8a9180924d5189a
Tim Zhang <tim.zhang@ringcentral.com>
2015-02-05 14:51:39 +0800

    add log

--- rule.go
+++ rule.go
@@ -1,5 +1,9 @@
 package sipt
 
+import (
+	"github.com/golang/glog"
+)
+
 type Rule struct {
 	matcher  *Matcher
 	replacer *Replacer
@@ -13,7 +17,9 @@ func NewRule(match, rMatch, replace string) *Rule {
 }
 
 func (r *Rule) Do(input []byte) []byte {
+	glog.Infof("try MATCH key:%s", r.matcher.String())
 	if r.matcher.match(input) {
+		glog.Infof("MATCH key:%s", r.matcher.String())
 		return r.replacer.replace(input)
 	}
 	return input
